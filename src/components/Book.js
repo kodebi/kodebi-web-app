804a5b91a99b10c69099637cdadb04aaf7cdee57
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-02-08 23:06:00 +0100

    experimental restyling of kodebi app

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Book = ({ _id, image, name, author, category }) => {
-    return (
-        <Link to={`/book/${_id}`} key={_id} className='book'>
-            <img src={image} alt={name} />
-            <footer className='book-info'>
+const Book = ({ _id, image, name }) => {
+  return (
+    <Link to={`/book/${_id}`} key={_id} className='book'>
+      <img src={image} alt={name} />
+      {/* <footer className='book-info'>
                 <h4>{name}</h4>
                 <h4 className='book-author'>{author}</h4>
                 <p className='book-genre'>{category}</p>
-            </footer>
-        </Link>
-    );
-};
+            </footer> */}
+    </Link>
+  )
+}
 
-export default Book;
+export default Book
--- src/components/UserAction.js
+++ src/components/UserAction.js
@@ -1,49 +1,50 @@
-import React from 'react';
-import { useAuthContext } from '../context/AuthContext';
-import ActionButton from './ActionBtn';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuthContext } from '../context/AuthContext'
+import ActionButton from './ActionBtn'
 
 const UserAction = ({ openBook, removeBook, openEditWindow, messageUser }) => {
-    const { userId } = useAuthContext();
-    const { username, condition, owner, status } = openBook;
+  const { userId } = useAuthContext()
+  const { username, condition, owner, status } = openBook
+  sessionStorage.setItem('bookOwnerId', owner)
+  sessionStorage.setItem('bookOwnerName', username)
 
-    return (
-        <>
-            <aside className='user-action'>
-                <section className='action-section'>
-                    <p>Dieses Buch gehört:</p>
-                    <h4>{username}</h4>
-                </section>
-                <hr className='separation-line' />
-                <section className='action-section'>
-                    <p>Zustand des Buches ist:</p>
-                    <h4>{condition}</h4>
-                </section>
-                <hr className='separation-line' />
-                <section className='action-section'>
-                    <p>Dieses Buch ist:</p>
-                    <h4>{status}</h4>
-                </section>
-                <hr className='separation-line' />
-                <section className='action-section'>
-                    <p>Was möchtest du tun?</p>
-                    {owner === userId ? (
-                        <>
-                            <ActionButton onClick={openEditWindow}>
-                                Jetzt bearbeiten
-                            </ActionButton>
-                            <ActionButton onClick={removeBook}>
-                                Jetzt löschen
-                            </ActionButton>
-                        </>
-                    ) : (
-                        <ActionButton onClick={messageUser}>
-                            Jetzt ausleihen
-                        </ActionButton>
-                    )}
-                </section>
-            </aside>
-        </>
-    );
-};
+  return (
+    <>
+      <aside className='user-action'>
+        <section className='action-section'>
+          <p>Dieses Buch gehört:</p>
+          <Link to={`/profile/${owner}`} className='username-link'>
+            {username}
+          </Link>
+        </section>
+        <hr className='separation-line' />
+        <section className='action-section'>
+          <p>Zustand des Buches ist:</p>
+          <h4>{condition}</h4>
+        </section>
+        <hr className='separation-line' />
+        <section className='action-section'>
+          <p>Dieses Buch ist:</p>
+          <h4>{status}</h4>
+        </section>
+        <hr className='separation-line' />
+        <section className='action-section'>
+          <p>Was möchtest du tun?</p>
+          {owner === userId ? (
+            <>
+              <ActionButton onClick={openEditWindow}>
+                Jetzt bearbeiten
+              </ActionButton>
+              <ActionButton onClick={removeBook}>Jetzt löschen</ActionButton>
+            </>
+          ) : (
+            <ActionButton onClick={messageUser}>Jetzt ausleihen</ActionButton>
+          )}
+        </section>
+      </aside>
+    </>
+  )
+}
 
-export default UserAction;
+export default UserAction
