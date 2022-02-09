1f2b304bdd04da20028bc4908e0821b95a2d7b53
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-02-09 11:48:52 +0100

    refactored styles of book cards

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Book = ({ _id, image, name }) => {
+const Book = ({ _id, image, name, author, category }) => {
   return (
     <Link to={`/book/${_id}`} key={_id} className='book'>
       <img src={image} alt={name} />
-      {/* <footer className='book-info'>
-                <h4>{name}</h4>
-                <h4 className='book-author'>{author}</h4>
-                <p className='book-genre'>{category}</p>
-            </footer> */}
+      <footer className='book-info'>
+        <h4>{name}</h4>
+        <h4 className='book-author'>{author}</h4>
+        <p className='book-genre'>{category}</p>
+      </footer>
     </Link>
   )
 }
