1ab3c07296cf45f1fa24912db42d7f071529a9eb
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-08-17 22:09:45 +0200

    updated bookbyuser route

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ export const API_REQUESTRESET = '/auth/requestPasswordReset/';
 export const API_RESETPASSWORD = '/auth/resetPassword';
 export const API_BOOKS = '/api/books/';
 export const API_USERS = '/api/users/';
-export const API_BOOKSBYUSER = '/api/books/user/';
+export const API_BOOKSBYUSER = '/api/book/user/';
 export const API_MESSAGES = '/api/messages/';
 export const API_MESSAGESUSER = '/api/messages/user/';
