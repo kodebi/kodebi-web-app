e56e2efc554be763e856dc29a78a1853f09aca27
Chris Schimetschka <chris.schimetschka@gmail.com>
2022-08-24 13:11:34 +0200

    created m-top utility class

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,51 +1,51 @@
-import * as React from 'react'
-import { InputField } from '../InputField'
-import { SigninBtn } from '../SigninBtn'
-import { Form } from '../Form'
-import { AuthContext } from '../../context/AuthContext'
+import * as React from 'react';
+import { InputField } from '../InputField';
+import { SigninBtn } from '../SigninBtn';
+import { Form } from '../Form';
+import { AuthContext } from '../../context/AuthContext';
 
 export const Login = ({ openPasswordResetTab }) => {
-  const {
-    userCredential: { email, password },
-    login,
-    checkSigninInput,
-  } = React.useContext(AuthContext)
+	const {
+		userCredential: { email, password },
+		login,
+		checkSigninInput,
+	} = React.useContext(AuthContext);
 
-  return (
-    <>
-      <Form className='form-center' onSubmit={login}>
-        <div className='title'>
-          <h3>Willkommen zurück</h3>
-        </div>
-        <section className='form'>
-          <InputField
-            type='text'
-            htmlFor='Deine Email:'
-            name='email'
-            id='email'
-            value={email}
-            onChange={checkSigninInput}
-            required
-          />
-          <InputField
-            type='password'
-            htmlFor='Dein Passwort:'
-            name='password'
-            id='password'
-            value={password}
-            onChange={checkSigninInput}
-            required
-          />
-          <SigninBtn type='submit'>Einloggen</SigninBtn>
-          <button
-            type='button'
-            className='reset'
-            onClick={openPasswordResetTab}
-          >
-            Passwort vergessen?
-          </button>
-        </section>
-      </Form>
-    </>
-  )
-}
+	return (
+		<>
+			<Form className="form-center" onSubmit={login}>
+				<div className="title">
+					<h3>Willkommen zurück</h3>
+				</div>
+				<section className="form">
+					<InputField
+						type="text"
+						htmlFor="Deine Email:"
+						name="email"
+						id="email"
+						value={email}
+						onChange={checkSigninInput}
+						required
+					/>
+					<InputField
+						type="password"
+						htmlFor="Dein Passwort:"
+						name="password"
+						id="password"
+						value={password}
+						onChange={checkSigninInput}
+						required
+					/>
+					<SigninBtn type="submit">Einloggen</SigninBtn>
+					<button
+						type="button"
+						className="reset"
+						onClick={openPasswordResetTab}
+					>
+						Passwort vergessen?
+					</button>
+				</section>
+			</Form>
+		</>
+	);
+};
